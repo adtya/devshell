57f2578f7419649001c0a6b85145ed88d1aaf71e
Adithya Nair <adtya@adtya.xyz>
2023-12-08 13:18:23 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A collection of handy dev-shells";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+    }
+  );
+}
