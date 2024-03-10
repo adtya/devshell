71f6f09dd1b0d9a401ebe04f6dbc5715e094d9d7
Adithya Nair <adtya@adtya.xyz>
2024-03-10 12:27:14 +0530

    use rspec from GEM_HOME if needed

--- shells/ruby.nix
+++ shells/ruby.nix
@@ -2,10 +2,10 @@
   buildInputs = with pkgs; [
     ruby_3_2
     postgresql_15
-    rubyPackages_3_2.rspec-core
   ];
   shellHook = ''
     export BUNDLE_HOME="$PWD/.nix/bundle"
     export GEM_HOME="$PWD/.nix/gem"
+    export PATH="GEM_HOME/bin:$PATH"
   '';
 }
